2a38eeb7c1e1672c43e08dbb31000dfe211a71ff
Chris Markle <markle976@gmail.com>
2013-06-09 16:38:40 -0400

    update sublime prefs and fix prompt

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Ask for the administrator password upfront
-sudo -v
-
 # move dotfiles to home dir
-sudo cp -R -i src/. $HOME
+cp -R -i src/. $HOME
 
 #reload
 source $HOME/.bash_profile
