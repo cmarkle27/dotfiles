0a7c8fa8da8e10cd0e0234cc11ca3ee8e223aa99
Chris Markle <cmarkle@markle976.turner.com>
2013-12-17 14:25:52 -0500

    Updated sublime icon replacement for ST3

--- sublime/mac_replace_icons.sh
+++ sublime/mac_replace_icons.sh
@@ -2,16 +2,16 @@
 
 # Default Sublime Text Location
 SUBLIME_PATH="/Applications"
-SUBLIME_NAME="Sublime Text 2.app"
+SUBLIME_NAME="Sublime Text.app"
 
 # The direction this script is running in
 THIS_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Replace the app icon (assumes default location, requires sudo)
-sudo cp "$THIS_DIR/Sublime Text 2.icns" "$SUBLIME_PATH/$SUBLIME_NAME/Contents/Resources/"
+sudo cp "$THIS_DIR/Sublime Text.icns" "$SUBLIME_PATH/$SUBLIME_NAME/Contents/Resources/"
 #sudo cp $THIS_DIR/Document\ Icons/*.icns "$SUBLIME_PATH/$SUBLIME_NAME/Contents/Resources/"
 
-SUBLIME_COPY="Sublime Text 2 COPY"
+SUBLIME_COPY="Sublime Text COPY"
 
 # Trickery to force icon replace
 sudo cp -r "$SUBLIME_PATH/$SUBLIME_NAME" "$SUBLIME_PATH/$SUBLIME_COPY"
